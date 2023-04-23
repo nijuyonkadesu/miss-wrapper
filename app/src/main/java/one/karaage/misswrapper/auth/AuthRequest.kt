0614a9065e5cb2e60e7efc108bdd3108ef53f9e6
nijuyonkadesu <24ksriramv@gmail.com>
2023-04-23 11:04:58 +0530

    Request, Response types

---
+++ app/src/main/java/one/karaage/misswrapper/auth/AuthRequest.kt
@@ -0,0 +1,6 @@
+package one.karaage.misswrapper.auth
+
+data class AuthRequest(
+    val username: String,
+    val password: String,
+)
---
+++ app/src/main/java/one/karaage/misswrapper/auth/TokenResponse.kt
@@ -0,0 +1,5 @@
+package one.karaage.misswrapper.auth
+
+data class TokenResponse(
+    val token: String
+)
