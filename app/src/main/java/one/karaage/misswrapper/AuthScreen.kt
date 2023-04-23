1d0f581dd8fce9b5365955aeafb3173af1de5b86
nijuyonkadesu <24ksriramv@gmail.com>
2023-04-23 19:26:08 +0530

    view model, channel, and observe in UI

--- app/src/main/java/one/karaage/misswrapper/AuthScreen.kt
+++ app/src/main/java/one/karaage/misswrapper/AuthScreen.kt
@@ -1,5 +1,6 @@
 package one.karaage.misswrapper.ui.theme
 
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -8,20 +9,56 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import one.karaage.misswrapper.AuthUiEvent
 import one.karaage.misswrapper.MainViewModel
+import one.karaage.misswrapper.auth.AuthResult
+import one.karaage.misswrapper.destinations.SecretScreenDestination
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@Destination(start = true)
 fun AuthScreen(
+    navigator: DestinationsNavigator,
     viewModel: MainViewModel = hiltViewModel()
 ) {
     val state = viewModel.state
+    val context = LocalContext.current
+    LaunchedEffect(viewModel, context){
+        viewModel.authResult.collect(){ result ->
+            when(result){
+                is AuthResult.Authorized -> {
+                    navigator.navigate(SecretScreenDestination){
+                        popUpTo(SecretScreenDestination.route){
+                            inclusive = true
+                        }
+                    }
+                }
+                is AuthResult.UnAuthorized -> {
+                    Toast.makeText(
+                        context,
+                        "Not Authorized 🫠",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                is AuthResult.UnknownError -> {
+                    Toast.makeText(
+                        context,
+                        "An unknown error has occured",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
--- app/src/main/java/one/karaage/misswrapper/MainViewModel.kt
+++ app/src/main/java/one/karaage/misswrapper/MainViewModel.kt
@@ -3,16 +3,77 @@ package one.karaage.misswrapper
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.autofill.AutofillType
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import one.karaage.misswrapper.auth.AuthRepository
+import one.karaage.misswrapper.auth.AuthResult
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(): ViewModel() {
+class MainViewModel @Inject constructor(
+    private val repository: AuthRepository,
+): ViewModel() {
 
     var state by mutableStateOf(AuthState())
 
-    fun onEvent(event: AuthUiEvent) {
+    // Custom channel to observe for our UI
+    private val resultChannel = Channel<AuthResult<Unit>>()
+    val authResult = resultChannel.receiveAsFlow()
 
+    init {
+        // to automatically log in, after first signIn
+        authenticate()
+    }
+    fun onEvent(event: AuthUiEvent) {
+        when(event){
+            is AuthUiEvent.SignInPasswordChanged ->
+                state = state.copy(signInPassword = event .value)
+            is AuthUiEvent.SignInUsernameChanged ->
+                state = state.copy(signInUsername = event .value)
+            AuthUiEvent.SignIn -> signIn()
+
+            is AuthUiEvent.SignUpPasswordChanged ->
+                state = state.copy(signUpPassword = event .value)
+            is AuthUiEvent.SignUpUsernameChanged ->
+                state = state.copy(signUpUsername = event .value)
+            AuthUiEvent.SignUp -> signUp()
+        }
+    }
+
+    private fun signUp(){
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.signUp(
+                state.signUpUsername,
+                state.signUpPassword
+            )
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
+    }
+
+    private fun signIn(){
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.signIn(
+                state.signInUsername,
+                state.signInPassword
+            )
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
+    }
+    private fun authenticate(){
+        viewModelScope.launch {
+            state = state.copy(isLoading = true)
+            val result = repository.authenticate()
+            resultChannel.send(result)
+            state = state.copy(isLoading = false)
+        }
     }
 }
