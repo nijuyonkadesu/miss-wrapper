bca5496a869ff1716c522bad853b454fba0733d6
nijuyonkadesu <24ksriramv@gmail.com>
2023-04-23 11:01:51 +0530

    State, Events, TextInput fields

---
+++ app/src/main/java/one/karaage/misswrapper/MainViewModel.kt
@@ -0,0 +1,18 @@
+package one.karaage.misswrapper
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(): ViewModel() {
+
+    var state by mutableStateOf(AuthState())
+
+    fun onEvent(event: AuthUiEvent) {
+
+    }
+}
